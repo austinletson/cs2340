2e5bca8163e6facca0bed880b9a526fccd61d33c
Alex Bolen <alex.bolen@rhinofamily.com>
2017-02-13 14:28:15 -0500

    Login Screen Layout Created)

---
+++ app/src/main/java/controllers/LoginActivity.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+/**
+ * Created by alex.bolen on 2/13/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
