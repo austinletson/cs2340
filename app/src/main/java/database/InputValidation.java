7d9da152acae982c5cbcddd7b1445b69a155aa4f
Courtney Pettiford <cpettiford@gatech.edu>
2017-03-29 23:54:51 -0400

    added database

---
+++ app/src/main/java/database/InputValidation.java
@@ -0,0 +1,8 @@
+package database;
+
+/**
+ * Created by cpettiford on 3/29/17.
+ */
+
+public class InputValidation {
+}
---
+++ app/src/main/java/model/SourceReportCondition.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by cpettiford on 3/29/17.
+ */
+
+public class SourceReportCondition {
+}
