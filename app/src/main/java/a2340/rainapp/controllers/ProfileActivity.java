26dc39df1eaf79470f046c3677a66b795f9a77fd
Courtney Pettiford <cpettiford@gatech.edu>
2017-04-02 17:47:33 -0400

    fixed some issues

--- app/src/main/java/a2340/rainapp/controllers/ProfileActivity.java
+++ app/src/main/java/a2340/rainapp/controllers/ProfileActivity.java
@@ -12,8 +12,6 @@ import database.UserDBHandler;
 import android.database.sqlite.SQLiteDatabase;
 
 
-import java.util.ArrayList;
-
 
 /**
  * Created by austinletson on 2/21/17.
@@ -30,16 +28,14 @@ public class ProfileActivity extends AppCompatActivity {
     private InputValidation inputValidation;
     private UserDBHandler userDBHandler;
 
-    private static final String SELECT_SQL = "SELECT email, address, title FROM users WHERE username = " + LoginActivity.loggedInUser;
+    private static final String SELECT_SQL =
+            "SELECT email, address, title FROM users WHERE username = "
+                    + LoginActivity.loggedInUser;
 
     private User user;
     private SQLiteDatabase db;
 
     private final AppCompatActivity activity = ProfileActivity.this;
-
-    private String tableName = userDBHandler.TABLE_USERS;
-
-
 
 
 
@@ -57,17 +53,20 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        if (LoginActivity.loggedInUserEmail != null && LoginActivity.loggedInUserEmail.length() > 0 ) {
+        if (LoginActivity.loggedInUserEmail != null
+                && LoginActivity.loggedInUserEmail.length() > 0 ) {
             emailEdit.setHint(LoginActivity.loggedInUserEmail);
 
         }
 
-        if (LoginActivity.loggedInUserAddress != null && LoginActivity.loggedInUserAddress.length() > 0) {
+        if (LoginActivity.loggedInUserAddress != null
+                && LoginActivity.loggedInUserAddress.length() > 0) {
             addressEdit.setHint(LoginActivity.loggedInUserAddress);
 
         }
 
-        if (LoginActivity.loggedInUserTitle != null && LoginActivity.loggedInUserTitle.length() > 0) {
+        if (LoginActivity.loggedInUserTitle != null
+                && LoginActivity.loggedInUserTitle.length() > 0) {
             titleEdit.setHint(LoginActivity.loggedInUserTitle);
 
         }
@@ -80,17 +79,20 @@ public class ProfileActivity extends AppCompatActivity {
         alertTextView = (TextView) findViewById(R.id.profileAlertView);
 
 
-        if (LoginActivity.loggedInUserEmail != null && LoginActivity.loggedInUserEmail.length() > 0 ) {
+        if (LoginActivity.loggedInUserEmail != null
+                && LoginActivity.loggedInUserEmail.length() > 0 ) {
             emailEdit.setHint(LoginActivity.loggedInUserEmail);
 
         }
 
-        if (LoginActivity.loggedInUserAddress != null && LoginActivity.loggedInUserAddress.length() > 0) {
+        if (LoginActivity.loggedInUserAddress != null
+                && LoginActivity.loggedInUserAddress.length() > 0) {
             addressEdit.setHint(LoginActivity.loggedInUserAddress);
 
         }
 
-        if (LoginActivity.loggedInUserTitle != null && LoginActivity.loggedInUserTitle.length() > 0) {
+        if (LoginActivity.loggedInUserTitle != null
+                && LoginActivity.loggedInUserTitle.length() > 0) {
             titleEdit.setHint(LoginActivity.loggedInUserTitle);
 
         }
@@ -109,31 +111,32 @@ public class ProfileActivity extends AppCompatActivity {
      * @param view
      */
     public void onUpdatePressed (View view) {
-        postDataToSQLite();
-        userDBHandler.getAllUsers();
-
+        storeProfileInfo();
     }
 
-    private void postDataToSQLite() {
-        if (!inputValidation.isEditTextFilled(addressEdit, alertTextView, getString(R.string.error_message_address))) {
+    private void storeProfileInfo() {
+        if (!inputValidation.isEditTextFilled(addressEdit, alertTextView,
+                getString(R.string.error_message_address))) {
             return;
         }
-        if (!inputValidation.isEditTextFilled(emailEdit, alertTextView, getString(R.string.error_message_email))) {
+        if (!inputValidation.isEditTextFilled(emailEdit, alertTextView,
+                getString(R.string.error_message_email))) {
             return;
         }
-        if (!inputValidation.isEditTextFilled(titleEdit, alertTextView, getString(R.string.error_message_title))) {
+        if (!inputValidation.isEditTextFilled(titleEdit, alertTextView,
+                getString(R.string.error_message_title))) {
             return;
         } else {
             alertTextView.setText("Your information has been saved.");
         }
 
 
-
         String addressInput = addressEdit.getText().toString();
         String emailInput = emailEdit.getText().toString();
         String titleInput = titleEdit.getText().toString();
 
-        boolean updateSuccessful = userDBHandler.updateUser(LoginActivity.loggedInUser, emailInput, addressInput, titleInput);
+        boolean updateSuccessful = userDBHandler.updateUser(LoginActivity.loggedInUser, emailInput,
+                addressInput, titleInput);
 
         if(updateSuccessful) {
             LoginActivity.loggedInUserEmail = emailInput;
